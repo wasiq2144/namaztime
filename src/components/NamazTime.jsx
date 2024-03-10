ab15d7a314841484df4a80639aca0868cf759a4a
wasiq2144 <wasiq2144@gmail.com>
2024-03-11 00:08:59 +0500

    new

--- src/components/NamazTime.jsx
+++ src/components/NamazTime.jsx
@@ -14,8 +14,6 @@ const NamazTable = () => {
   const dispatch = useDispatch();
   const { prayerTimes, error } = useSelector((state) => state.namaz);
   const { cityName, countryName } = useSelector((state) => state.location);
-  const [nextPrayer, setNextPrayer] = useState('');
-  const [timeLeft, setTimeLeft] = useState('');
 
   useEffect(() => {
     if (cityName && countryName) {
@@ -23,44 +21,6 @@ const NamazTable = () => {
     }
   }, [dispatch, cityName, countryName]);
 
-  useEffect(() => {
-    if (!prayerTimes) return;
-
-    const currentTime = new Date();
-    const currentTimeString = `${currentTime.getHours()}:${currentTime.getMinutes() < 10 ? '0' : ''}${currentTime.getMinutes()}`;
-
-    let nextPrayerTime = null;
-    let minDifference = Infinity;
-    let nextPrayer = '';
-
-    for (const [prayer, time] of Object.entries(prayerTimes)) {
-      if (time > currentTimeString) {
-        const [hour, minute] = time.split(':').map(Number);
-        const prayerDate = new Date();
-        prayerDate.setHours(hour, minute, 0, 0);
-        const difference = prayerDate.getTime() - currentTime.getTime();
-        if (difference < minDifference) {
-          minDifference = difference;
-          nextPrayerTime = prayerDate;
-          nextPrayer = prayer;
-        }
-      }
-    }
-
-    setNextPrayer(nextPrayer);
-
-    const intervalId = setInterval(() => {
-      const difference = nextPrayerTime.getTime() - Date.now();
-      if (difference <= 0) {
-        clearInterval(intervalId);
-        setTimeLeft('');
-      } else {
-        setTimeLeft(difference);
-      }
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [prayerTimes]);
 
   if (!prayerTimes) {
     return <div>Loading...</div>;
@@ -70,17 +30,8 @@ const NamazTable = () => {
     return <div>Error: {error.message || 'Unknown Error'}</div>;
   }
 
-  const secondsLeft = Math.floor(timeLeft / 1000);
-  const minutesLeft = Math.floor(secondsLeft / 60);
-  const hoursLeft = Math.floor(minutesLeft / 60);
-
   return (
     <div className='dark:text-light-secondary text-dark-secondary text-center mt-5'>
-      {nextPrayer && timeLeft !== '' && (
-        <div className='font-semibold py-2 sm:text-lg text-sm text-right'>
-          {nextPrayer} in {hoursLeft} hours {minutesLeft % 60} minutes
-        </div>
-      )}
       <h2 className='sm:text-xl text-lg'>Namaz Times</h2>
       <table className='table-auto sm:w[70%] w-[100%]  text-center mx-auto'>
         <thead>
